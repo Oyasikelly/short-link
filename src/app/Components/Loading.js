896870c7b533292d5365ffcce0416146f5034a71
Oyasi Kelly <104142584+Oyasikelly@users.noreply.github.com>
2024-12-07 15:37:09 +0100

    Update Loading.js

--- src/app/Components/Loading.js
+++ src/app/Components/Loading.js
@@ -2,7 +2,7 @@
 
 export default function Loading() {
   return (
-    <div className="fixed flex justify-center top-0 left-0 right-0 z-30  bg-base-400 backdrop-blur-[2px] w-full h-screen">
+    <div className="fixed flex justify-center top-0 left-0 right-0 z-20  bg-base-400 backdrop-blur-[2px] w-full h-screen">
       <span className="loading loading-dots loading-xs"></span>
       <span className="loading loading-dots loading-sm"></span>
       <span className="loading loading-dots loading-md"></span>
